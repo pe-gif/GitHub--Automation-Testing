524e0b04b387d87aa390f0d1883344e48e4c08d7
pe-gif <153048514+pe-gif@users.noreply.github.com>
2025-04-21 14:02:53 -0400

    Update Issuestest.java
    added comments

--- Issuestest.java
+++ Issuestest.java
@@ -21,7 +21,8 @@ public class Tests {
         driver.get("https://www.google.com");
         driver.manage().window().maximize();
         driver.get("https://www.github.com/login");
-
+        
+        //login into github account
         driver.findElement(By.id("login_field")).sendKeys("githubemailtest@gmail.com");
         Thread.sleep(500);
 
@@ -34,17 +35,20 @@ public class Tests {
     }
 
     @Test
-    public void test1() throws Exception {
+    public void Create_Issue() throws Exception {
         System.setProperty("webdriver.google.driver", "C:\\Users\\peter\\Downloads\\chromedriver-win64\\chromedriver.exe");
+        //opens issue github page
         driver.get("https://github.com/issues/assigned");
         Thread.sleep(2000);
 
+        //begin creating a issue
         driver.findElement(By.cssSelector("div.Box-sc-g0xbh4-0.evWEbn")).click();
         Thread.sleep(2000);
 
         driver.findElement(By.cssSelector("div.Box-sc-g0xbh4-0.prc-ActionList-ItemDescriptionWrap-VJA7h")).click();
         Thread.sleep(2000);
 
+        //naming issue
         driver.findElement(By.id(":r23:")).sendKeys("This is a test issue");
         Thread.sleep(2000);
 
@@ -52,16 +56,18 @@ public class Tests {
     }
 
     @Test
-    public void test2() throws Exception {
+    public void Assign_To() throws Exception {
         driver.get("https://github.com/issues/assigned");
         Thread.sleep(1000);
 
+        //navigate to created issue
         driver.findElement(By.xpath("//div[text()='Created by me']")).click();
         Thread.sleep(1000);
 
         driver.findElement(By.xpath("//a[text()='This is a test issue']")).click();
         Thread.sleep(1000);
 
+        //opens assign box and assigns self
         driver.findElement(By.cssSelector("div.Box-sc-g0xbh4-0.gQxdEn")).click();
         Thread.sleep(1000);
 
@@ -71,22 +77,25 @@ public class Tests {
     }
 
     @Test
-    public void test3() throws Exception {
+    public void Create_Sub() throws Exception {
         driver.get("https://github.com/issues/assigned");
         Thread.sleep(1000);
 
+        //navigate to created issue
         driver.findElement(By.xpath("//div[text()='Created by me']")).click();
         Thread.sleep(1000);
 
         driver.findElement(By.xpath("//a[text()='This is a test issue']")).click();
         Thread.sleep(1000);
 
+        //begin creating a sub issue
         driver.findElement(By.xpath("//span[text()='Create sub-issue']")).click();
         Thread.sleep(1000);
 
         driver.findElement(By.xpath("//span[text()='Create a new issue from scratch']")).click();
         Thread.sleep(2000);
 
+        //names the sub issue
         driver.findElement(By.className("prc-components-Input-Ic-y8")).sendKeys("This is a issue of the issue");
         Thread.sleep(2000);
 
@@ -94,7 +103,7 @@ public class Tests {
     }
 
     @Test
-    public void test4() throws Exception {
+    public void Comment() throws Exception {
         driver.get("https://github.com/issues/assigned");
         Thread.sleep(1000);
 
@@ -107,15 +116,17 @@ public class Tests {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0, 1000)");
 
+        //opens up a comment section in issue and types the issue
         driver.findElement(By.xpath("//textarea[@class='prc-Textarea-TextArea-13q4j MarkdownInput-module__textArea--QjIwG']")).sendKeys("Test issue is completedddd");
         Thread.sleep(1000);
 
+        //comment button
         driver.findElement(By.xpath("//span[text()='Comment']")).click();
         Thread.sleep(1000);
     }
 
     @Test
-    public void test5() throws Exception {
+    public void Deletes() throws Exception {
         driver.get("https://github.com/issues/assigned");
         Thread.sleep(1000);
 
@@ -125,9 +136,11 @@ public class Tests {
         driver.findElement(By.xpath("//a[text()='This is a test issue']")).click();
         Thread.sleep(1000);
 
+        //Scroll down to delete key
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0, 1000)");
 
+        //Deletes and confirms the issue 
         driver.findElement(By.xpath("//span[text()='Delete issue']")).click();
         Thread.sleep(1000);
 
